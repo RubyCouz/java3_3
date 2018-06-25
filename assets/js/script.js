c83da9fab84bb6290755cbd07a63b4b7df67915c
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-06-25 14:28:09 +0200

    exo3 partie3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function textGreen() {
+  document.getElementById('text').style.color = 'green';
+}
+function textRed() {
+  document.getElementbyId('text').style.color = 'red';
+}
+function textBlue() {
+  document.getElementById('text').style.color = 'blue';
+}
